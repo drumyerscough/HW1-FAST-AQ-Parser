59626c726bc3c8c2d7bfae98b80fe8fe256355e9
drumyerscough <68093667+drumyerscough@users.noreply.github.com>
2024-01-14 13:58:12 -0800

    wrote code for example.py, updated pyproject.toml

--- example.py
+++ example.py
@@ -3,27 +3,36 @@ from seqparser import (
         FastqParser,
         transcribe,
         reverse_transcribe)
+import os
 
 def main():
     """
     The main function
     """
     # Create instance of FastaParser
+    fa_parser = FastaParser(os.path.join('data', 'test.fa'))
     # Create instance of FastqParser
-        
+    fq_parser = FastqParser(os.path.join('data', 'test.fq'))
+
     # For each record of FastaParser, Transcribe the sequence
     # and print it to console
-       
+    for record in fa_parser:
+        print(record[0], transcribe(record[1]))
+    
     # For each record of FastqParser, Transcribe the sequence
     # and print it to console
-
+    for record in fq_parser:
+        print(record[0], transcribe(record[1]))
 
     # For each record of FastaParser, Reverse Transcribe the sequence
     # and print it to console
-       
+    for record in fa_parser:
+        print(record[0], reverse_transcribe(record[1]))
+
     # For each record of FastqParser, Reverse Transcribe the sequence
     # and print it to console
-
+    for record in fq_parser:
+        print(record[0], reverse_transcribe(record[1]))
 
 """
 When executing a python script from the command line there will
--- seqparser/seq.py
+++ seqparser/seq.py
@@ -10,13 +10,19 @@ def transcribe(seq: str, reverse: bool = False) -> str:
     Write a function that will transcribe (replace DNA sequence to RNA
     by replacing all 'T' to 'U') in an input sequence
     """
-    pass
+    new_seq = str()
+    for nuc in seq:
+        assert nuc in ALLOWED_NUC
+        new_seq += TRANSCRIPTION_MAPPING[nuc]
+
+    if reverse:
+        return new_seq[::-1]
+    else:
+        return new_seq
 
 def reverse_transcribe(seq: str) -> str:
     """
     Write a function that will transcribe an input sequence and reverse
     the sequence
     """
-    # Hey this is my comment
-    # Again!
-    pass
+    return transcribe(seq, reverse=True)
